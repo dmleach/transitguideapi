b76e092eaaf5ab16dd2543e3ff0a84f6001c9612
Dave Leach <dave@daveleach.work>
2016-12-28 10:18:10 -0500

    Moved get method from PlaceController to DataActionController

--- controller/PlaceController.php
+++ controller/PlaceController.php
@@ -4,14 +4,6 @@ namespace transitguide\api\controller;
 
 class PlaceController extends base\DataActionController
 {
-    public function get($parameters)
-    {
-        // echo "<p>This is PlaceController::get</p>";
-        // echo '<pre>' . print_r($parameters, true) . '</pre>';
-
-        return $this->model->select($parameters);
-    }
-
     public function getModelClassName(): string
     {
         return '\transitguide\api\model\PlaceModel';
--- controller/base/DataActionController.php
+++ controller/base/DataActionController.php
@@ -8,8 +8,22 @@ abstract class DataActionController extends ActionController
 
     public function __construct(\transitguide\api\controller\db\ConfigController $config)
     {
+        // Get the name of this controller's model class from the child
         $modelClassName = $this->getModelClassName();
+
+        // Use the provided database configuration to instantiate the model
         $this->model = new $modelClassName($config);
+    }
+
+    public function get($parameters)
+    {
+        // Call either the select or list function, depending on whether any
+        // parameters were provided 
+        if ($parameters) {
+            return $this->model->select($parameters);
+        } else {
+            return $this->model->list();
+        }
     }
 
     public abstract function getModelClassName(): string;
